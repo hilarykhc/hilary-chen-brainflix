ec8d5800ece12b0f3eff96c90c902fc701f97fba
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-01-17 14:35:33 -0500

    finalize header components in mobile, tablet and desktop screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,71 +10,12 @@ export default function Header() {
       </a>
 
       <div class="header__search">
-        <input type="text" class="header__search-input" placeholder="Search" />
-        <img src={avatar} alt="Mohan-muruge" class="header__search-avatar" />
+        <input type="text" class="header__input" placeholder="Search" />
+        <img src={avatar} alt="Mohan-muruge" class="header__avatar" />
         <a class="header__upload-btn upload-btn" href="#">
           UPLOAD
         </a>
       </div>
     </header>
-
-    /*
-
-    <header className="header">
-      <div className="header__wrapper">
-        <a className="header__link" href="#">
-          <img
-            className="header__logo"
-            src={brainFlixLogo}
-            alt="BrainFlix logo"
-          />
-        </a>
-
-        <form className="search">
-          <div className="search__wrapper--mobile">
-            <div className="search__wrapper">
-              <label htmlFor="search" className="search__label">
-                <img
-                  src={searchIcon}
-                  alt="search icon"
-                  className="search__icon"
-                />
-              </label>
-              <input
-                id="search"
-                type="text"
-                className="search__input" //add img as background-img to .search__input
-                placeholder="Search"
-              />
-            </div>
-
-            <button className="search__btn search__btn--tablet" type="submit">
-              <img
-                src={uploadButton}
-                alt="upload icon"
-                className="search__upload-icon"
-              />
-              UPLOAD
-            </button>
-
-            <div className="search__avatar-wrapper">
-              <img src={avatar} alt="Mohan-muruge" className="search__avatar" />
-            </div>
-          </div>
-          <button className="search__btn" type="submit">
-            <img
-              src={uploadButton}
-              alt="upload icon"
-              className="search__btn-icon"
-            />
-            UPLOAD
-          </button>
-        </form>
-
-    
-      </div>
-    </header>
-
-    */
   );
 }
