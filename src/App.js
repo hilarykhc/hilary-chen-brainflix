e538be609e395f10c26a87e1b347ab76843a84ef
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-01-20 13:41:47 -0500

    import json files to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import videoDetailsData from "../src/data/video-details.json";
+import videoData from "../src/data/videos.json";
 import VideoDetails from "./components/VideoDetails/VideoDetails";
 import Comments from "./components/Comments/Comments";
 import VideoList from "./components/VideoList/VideoList";
