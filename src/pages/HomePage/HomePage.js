18484e9275a4ed8a44fa3545741c86ad39cb5341
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-01-27 22:02:07 -0500

    updated homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import VideoDetails from "../../components/VideoDetails/VideoDetails";
 import Comments from "../../components/Comments/Comments";
 import VideoList from "../../components/VideoList/VideoList";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function HomePage() {
@@ -11,12 +11,13 @@ export default function HomePage() {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState([]);
   const { videoId = "" } = useParams();
+  //   const navigate = useNavigate();
 
   const baseUrl = "https://project-2-api.herokuapp.com/videos";
   const apiKey = "2bd1529e-9731-4ea5-974c-934934bdc239";
 
-  const searchSingleVideo = (videoId) =>
-    `${baseUrl}/${videoId}?api_key=${apiKey}`;
+  //   const searchSingleVideo = (videoId) =>
+  //     `${baseUrl}/${videoId}?api_key=${apiKey}`;
 
   // videoId to find selected video
   const selectedVideoFromParams = videos.find((video) => video.id === videoId);
@@ -28,41 +29,90 @@ export default function HomePage() {
     (video) => video.id !== selectedVideo?.id
   );
 
-  // unwrapping useEffect
-  const fetchVideos = async () => {
-    try {
-      const response = await axios.get(`${baseUrl}?api_key=${apiKey}`);
-      setVideos(response.data);
-      if (!videoId && response.data.length > 0) {
-        fetchSingleVideo(response.data[0].id);
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}?api_key=${apiKey}`);
+        setVideos(response.data);
+
+        const singleVideo = videoId || "84e96018-4022-434e-80bf-000ce4cd12b8";
+        const singleVideoRes = await axios.get(
+          `${baseUrl}/${singleVideo}?api_key=${apiKey}`
+        );
+        setSelectedVideo(singleVideoRes.data);
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    };
 
-  useEffect(() => {
     fetchVideos();
-  }, []);
-
-  const fetchSingleVideo = async (videoId) => {
-    try {
-      const response = await axios.get(
-        `${baseUrl}/${videoId}?api_key=${apiKey}`
-      );
-      setSelectedVideo(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    if (videoId) {
-      fetchSingleVideo(videoId || "84e96018-4022-434e-80bf-000ce4cd12b8"); // pass id for default.
-    }
   }, [videoId]);
 
-  //
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await axios.get(`${baseUrl}?api_key=${apiKey}`);
+  //         setVideos(response.data);
+
+  //         const videoToFetch = videoId || "84e96018-4022-434e-80bf-000ce4cd12b8";
+  //         const singleVideoResponse = await axios.get(
+  //           `${baseUrl}/${videoToFetch}?api_key=${apiKey}`
+  //         );
+  //         setSelectedVideo(singleVideoResponse.data);
+  //       } catch (error) {
+  //         console.error(error);
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }, [videoId]);
+
+  // unwrapping useEffect------------------------------------------------------
+  //   const fetchVideos = async () => {
+  //     try {
+  //       const response = await axios.get(`${baseUrl}?api_key=${apiKey}`);
+  //       setVideos(response.data);
+  //       if (!videoId && response.data.length > 0) {
+  //         const defaultVideoId = "84e96018-4022-434e-80bf-000ce4cd12b8";
+  //         //   navigate(`/videos/${defaultVideoId}`);
+  //         fetchSingleVideo(defaultVideoId);
+  //       } else {
+  //         fetchSingleVideo(videoId);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     fetchVideos();
+  //   }, [videos]); // add [videoId, videos] directs to homepage, but flickers. why is it flickering?
+
+  //--------------------------------------------------------------here
+  //   const fetchSingleVideo = async (videoId) => {
+  //     try {
+  //       const response = await axios.get(
+  //         `${baseUrl}/${videoId}?api_key=${apiKey}`
+  //       );
+  //       setSelectedVideo(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     fetchSingleVideo(videoId || "84e96018-4022-434e-80bf-000ce4cd12b8");
+  //   }, [videoId]);
+
+  //--------------------------------------------------------------here
+
+  //   //didn't need this??
+  // useEffect(() => {
+  //   if (videoId) {
+  //     fetchSingleVideo(videoId || "84e96018-4022-434e-80bf-000ce4cd12b8"); // pass id for default.
+  //   }
+  // }, [videoId]);
+  // unwrapping useEffect------------------------------------------------------
 
   // commenting out lines 66 to 105 -----------------------------------------
   // get all videos
@@ -132,7 +182,7 @@ export default function HomePage() {
           <Comments selectedVideo={selectedVideo.comments} />
         </div>
         <div className="div-container3">
-          <VideoList videos={videos} selectVideo={handleSelectVideo} />
+          <VideoList videos={filteredVideos} selectVideo={handleSelectVideo} />
         </div>
       </div>
     </div>
