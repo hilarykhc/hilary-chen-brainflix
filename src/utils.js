23844b5f5064c24df745972dabec66f3685458da
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-02 09:08:48 -0500

    add utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export const fetchVideos = async () => {
   try {
     const response = await axios.get("http://localhost:8080/videos");
+    console.log(response.data);
+    setVideosData(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
